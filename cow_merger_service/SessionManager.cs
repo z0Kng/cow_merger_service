8e06ffdc1079bb0815b98a49640dfd33d9d8d543
Michael Scherle <michael.scherle@rz.uni-freiburg.de>
2024-04-22 15:35:24 +0200

    fixed wrong legth

--- cow_merger_service/SessionManager.cs
+++ cow_merger_service/SessionManager.cs
@@ -86,7 +86,7 @@ namespace cow_merger_service
                     spanDataBlockIndex++;
                 }
             }
-            session.DataFileStream.SetLength(metaData.Offset+session.BitfieldSize*4096);
+            session.DataFileStream.SetLength(metaData.Offset+session.BitfieldSize*8*4096);
         }
 
         private bool UpdateBlock(CowSession session, int blockNumber, Span<byte> bitfield, Span<byte> spanData)
